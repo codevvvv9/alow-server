687621e2e1bf46d8e438eb70518a08e2fa9b354f
shawnwwu <wslsdust@163.com>
2025-06-03 16:53:24 +0800

    feat: :recycle: 三层架构修改目录结构

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -9,9 +9,9 @@ import {
   HttpStatus,
   Query,
 } from '@nestjs/common';
-import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { UserService } from '../services/user.service';
+import { CreateUserDto } from '../dtos/create-user.dto';
+import { UpdateUserDto } from '../dtos/update-user.dto';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import { PaginationParamsDto } from 'src/shared/dtos/pagination-params.dto';
@@ -43,6 +43,9 @@ export class UserController {
     return this.userService.create(createUserDto);
   }
 
+  @ApiOperation({
+    summary: '获取所有用户',
+  })
   @Get()
   async findAll(@Query() query: Record<string, any>) {
     // 如果需要处理分页参数，可以在这里进行处理
@@ -62,16 +65,33 @@ export class UserController {
     }
   }
 
+  @ApiOperation({
+    summary: '获取单个用户',
+    description: '根据ID获取用户信息',
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
   }
 
+  @ApiOperation({
+    summary: '更新用户信息',
+    description: '根据ID更新用户信息',
+  })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
   }
 
+  @ApiOperation({
+    summary: '删除用户',
+    description: '根据ID删除用户',
+  })
+  @ApiResponse({
+    status: HttpStatus.NO_CONTENT,
+    description: '用户删除成功',
+    // type: null, // 删除操作通常不返回数据
+  })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -1,9 +1,9 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { SystemService } from '../shared/system.service'; // Adjust the path as needed
+import { CreateUserDto } from '../dtos/create-user.dto';
+import { UpdateUserDto } from '../dtos/update-user.dto';
+import { SystemService } from '../../shared/system.service'; // Adjust the path as needed
 import { MongoRepository } from 'typeorm';
-import { User } from './entities/user.mongo.entity';
+import { User } from '../entities/user.mongo.entity';
 import { PaginationParamsDto } from 'src/shared/dtos/pagination-params.dto';
 import { AppLoggerService } from 'src/shared/logger/logger.service';
 
